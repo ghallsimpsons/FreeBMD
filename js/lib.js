3199b589c198941dc1563fb39608b10a35470eec
unknown <grantlandhall@grantlandhall.com>
2014-04-06 03:23:33 -0700

    Variable Retrieval

--- js/lib.js
+++ js/lib.js
@@ -36,6 +36,12 @@ var env = {
 		},
 	};
 
+function load_locals(){
+	for (var localvar in env.vars){
+		math[localvar]=env.vars[localvar].val;
+	}
+}
+	
 function isBareword( word ){
 	if(word.match(/^[a-zA-Z]\w*$/)) return true; else return false;
 }
@@ -173,6 +179,7 @@ function exec_statement( line ){
 			tmpvar['val'] = eval_expr( split_line.slice(split_line.indexOf('=')+1).join('') );
 			tmpvar['type']='scalar';
 			env.vars[varname]=tmpvar;
+			math[varname]=tmpvar.val;
 			return env.vars[varname]['val'];
 			}
 			//else if( /*obj_prop*/ ){
@@ -189,10 +196,6 @@ function exec_statement( line ){
 	return err;
 	}
 	//math.eval(line);
-}
-
-function storeLocal(varname, val){
-	math.eval(varname+"="+val);
 }
 
 function eval_expr( expr ){
