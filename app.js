240ed365bc72dcb7304deb2b6b7947a3b9f4c3bc
Richard Sheperd <rjsheperd@gmail.com>
2014-04-06 05:11:57 -0700

    implemented views

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
 app.get('/', function(request, response) {
-	return response.render('index', {name: 1});
+	return response.render('index', {env: null});
 });
 
 app.post('/save', function(request, response) {
