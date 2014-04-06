2ca656a39c08c584c4f9f916361eae561d8e3e4c
unknown <grantlandhall@grantlandhall.com>
2014-04-06 02:16:51 -0700

    Lots of parsing features

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,7 @@
 var math=mathjs();
 
+var parse_helper={};
+
 var env = {
 	'uid': false/*{{ uid }}*/ ,
 	
@@ -18,21 +20,26 @@ var env = {
 		/*{% if vars %}
 		{{ vars }}
 		{% else %}*/
-		e: math.e,
-		pi: math.pi,
-		i: math.sqrt(-1),
+		'e': {
+			'type': "scalar",
+			'val': math.e,
+			},
+		'pi': {
+			'type': 'scalar',
+			'val': math.pi,
+			},
+		'i': {
+			'type': 'scalar',
+			'val': math.sqrt(-1),
+			},
 		//{% endif %}
-		},
-	
-	'history': {
-		'0': "init()",
 		},
 	};
 
-var binary_ops = ['+', '-', '*', '/', '^', '&', '|', '&&', '||'];
-var comparison_ops = ['>','<','<=','>=','==','~=',];
-
-
+function isBareword( word ){
+	if(word.match(/^[a-zA-Z]\w*$/)) return true; else return false;
+}
+	
 function flatten(array) {
   var result = [], self = arguments.callee;
   array.forEach(function(item) {
@@ -44,13 +51,91 @@ function flatten(array) {
   return result;
 };
 
+function next_semantic_block( obj, index ){
+	//Returns the start and end index of the next semantic block [start,end]
+	var start_ind;
+	var blocked='';
+	for(var i=index; i<obj.length; i++){
+		console.log(blocked);
+		if(blocked){
+			switch(blocked)
+			{
+				case '[':
+					if(obj[i]==']') return [start_ind, i+1];
+					break;
+				case '(':
+					console.log("This worked!");
+					if(obj[i]==')') return [start_ind, i+1];
+					break;
+				case '{':
+					if(obj[i]=='}') return [start_ind, i+1];
+					break;
+			}
+		}
+		else if (isBareword(obj[i])) return [i,i+1];
+		else {
+			console.log(obj[i]+":"+(obj[i]=='('));
+			if (obj[i]=='['){ blocked='['; start_ind=i; }
+			else if(obj[i]=='('){ blocked='('; start_ind=i; }
+			else if(obj[i]=='{'){ blocked='{'; start_ind=i; }
+		}
+	}
+}
+
+var binary_ops = ['+', '-', '*', '/', '^', '&', '|', '&&', '||'];
+var comparison_ops = ['>','<'];
+var specials = [',', '=', '[', ']', ';', '~', '(', ')'];
+
+var all_tokens = binary_ops;
+all_tokens.push(comparison_ops);
+all_tokens.push(specials);
+all_tokens = flatten(all_tokens);
+
+function preparse( str ){
+	var i=0;
+	var saved=0;
+	var escaped_index;
+	var parsed_str=str;
+	var escaped='f';
+	while (i<parsed_str.length){
+		if (escaped=='f'){
+			if(parsed_str[i]=='"'){
+				escaped='"';
+				escaped_index=i;
+			}
+			else if(parsed_str[i]=="'"){
+				escaped="'";
+				escaped_index=i;
+			}
+			else if(parsed_str[i]=='%'){
+				parsed_str=parsed_str.slice(0,i);
+				break;
+			}
+			i++;
+		}
+		else if(parsed_str[i]==escaped){
+			parse_helper['%'+saved+'%']=parsed_str.slice(escaped_index,i+1);
+			parsed_str=parsed_str.slice(0,escaped_index)+"%"+saved+"%"+parsed_str.slice(i+1,parsed_str.length);
+			i=escaped_index+("%"+saved+"%").length;
+			saved++;
+			escaped='f';
+		}
+		else i++;
+		}
+	return parsed_str;
+}
+
+function postparse( str ){
+	return str.replace(/%(\w)%/g,function(a,b){return parse_helper['%'+b+'%'];});
+	}
+
 function tokenize_str( str, token ){
-	console.log("str:"+typeof(str));
 	var sarr=[];
 	var spl=String(str).split(token);
 	for (var i=0; i<spl.length-1; i++){
 		if (spl[i]!='')
 		sarr.push(spl[i]);
+		if(token!=' ')
 		sarr.push(token);
 	}
 	sarr.push(spl[spl.length-1]);
@@ -64,18 +149,57 @@ function tokenize( line, tokens ){
 		tmparr=flatten(arr);
 		arr=[];
 		for (var str in tmparr){
-		console.log(tokens[t] + "  :  "+tmparr[str]);
 			arr.push(tokenize_str( tmparr[str], tokens[t] ));
-			//alert(tmparr+'\n'+tokens[t]+" : "+arr);
 		}
 	}
 	return flatten(arr);
 }
 
 function exec_statement( line ){
-	if( ) {
-
+	try{
+		parsed_line=preparse(line);
+		split_line = tokenize(parsed_line, all_tokens);
+		for(var i in split_line){
+			split_line[i]=postparse(split_line[i]);
+		}
+		console.log(split_line);
+		if(split_line[0]=="for"){
+		}
+		else if (split_line.indexOf('=')>0 && split_line[split_line.indexOf('=')+1]!='='){
+			//Yay! We've found an assignment!
+			if( 1==1 ){
+			varname=split_line.slice(0,split_line.indexOf('=')).join('');
+			tmpvar={};
+			tmpvar['val'] = eval_expr( split_line.slice(split_line.indexOf('=')+1).join('') );
+			tmpvar['type']='scalar';
+			env.vars[varname]=tmpvar;
+			return env.vars[varname]['val'];
+			}
+			//else if( /*obj_prop*/ ){
+				
+			//}
+			//else if( /*matrix*/ ){
+			
+			//}
+		}
+		return eval_expr(line);
+	
 	}
-	math.eval(line);
+	catch(err){
+	return err;
+	}
+	//math.eval(line);
+}
+
+function storeLocal(varname, val){
+	math.eval(varname+"="+val);
+}
+
+function eval_expr( expr ){
+	console.log("expr: "+expr);
+	try{
+		return math.eval(expr);
+	}
+	catch(err){ return expr };
 }
 
