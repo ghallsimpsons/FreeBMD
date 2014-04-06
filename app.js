a298fbab63ed941b9f48349ed7b5e059b8200d2e
Richard Sheperd <rjsheperd@gmail.com>
2014-04-05 23:48:04 -0700

    heroku standard node.js server

--- app.js
+++ app.js
@@ -31,10 +31,7 @@
 
 // web.js
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
-
-app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
