da66a805a2ecff0f77304c60d1495e2abf52e031
Richard Sheperd <rjsheperd@gmail.com>
2014-04-05 23:50:23 -0700

    node static loader

--- app.js
+++ app.js
@@ -33,9 +33,10 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(
+     "/",
+     express.static(__dirname)
+);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
