6ae618ef944809c309ce05107da2e24bce860a69
Richard Sheperd <rjsheperd@gmail.com>
2014-04-06 07:10:32 -0700

    fix matrix value data

--- public/js/lib.js
+++ public/js/lib.js
@@ -242,8 +242,8 @@ function exec_statement( line ){
 			tmpvar['val'] = eval_expr( expr.join('') );
 			tmpvar['type']='scalar';
 			if(tmpvar.val.hasOwnProperty("_data"))
-				env.vars[varname]=tmpvar._data;
-			else env.vars[varname]=tmpvar;
+				tmpvar.val=tmpvar.val._data;
+			env.vars[varname]=tmpvar;
 			math[varname]=tmpvar.val;
 			return env.vars[varname]['val'];
 			}
