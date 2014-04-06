a5f7242c9a4887aea872d7af9406d0707dd2774f
Richard Sheperd <rjsheperd@gmail.com>
2014-04-06 05:29:04 -0700

    implemented views

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/:secret', function(request, response) {
 			if(result.rows.length == 0) return response.redirect("/");
 			var _env = JSON.parse(result.rows[0].environment);
 			console.log(_env);
-			return response.render('index',{env: _env);
+			return response.render('index',{env: _env});
 		});
 	});
 });
